5dcef2099f6c0996b4ab3da48daa2d2bb77e2533
c9s <cornelius.howl@gmail.com>
2012-01-08 04:27:49 +0800

    Flavor Installer command.

--- src/GenPHP/Application.php
+++ src/GenPHP/Application.php
@@ -14,6 +14,7 @@ class Application extends CLIApp
         $this->registerCommand('new');
         $this->registerCommand('list');
         $this->registerCommand('init');
+        $this->registerCommand('install');
         parent::init();
     }
 
--- src/GenPHP/Command/InstallCommand.php
+++ src/GenPHP/Command/InstallCommand.php
@@ -1,5 +1,8 @@
 <?php
 namespace GenPHP\Command;
+use GenPHP\Flavor\FlavorDirectory;
+use GenPHP\Path;
+use GenPHP\Operation\Helper;
 
 class InstallCommand extends \CLIFramework\Command 
 {
@@ -9,8 +12,20 @@ class InstallCommand extends \CLIFramework\Command
 
     function execute($nameOrPath)
     {
+        $logger = $this->getLogger();
+        $homePath = Path::get_home_flavor_path();
+        if( file_exists($nameOrPath) ) {
+            Helper::mktree( $homePath );
 
-
-
+            $flavor = new FlavorDirectory($nameOrPath);
+            $logger->info( "Installing " . $flavor->getName() . "..." );
+            Helper::copy_dir( 
+                $flavor->__toString(),
+                $homePath . DIRECTORY_SEPARATOR . $flavor->getName(), 
+                function($target) use ($logger) {
+                    $logger->info( "Installing " . $target,1);
+            });
+        }
+        $logger->info("Done");
     }
 }
