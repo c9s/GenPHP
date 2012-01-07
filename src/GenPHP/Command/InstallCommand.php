410f53b46d55334425b77d5ec88fe26c64ef0379
c9s <cornelius.howl@gmail.com>
2012-01-08 02:56:04 +0800

    Flavor installer

---
+++ src/GenPHP/Command/InstallCommand.php
@@ -0,0 +1,14 @@
+<?php
+namespace GenPHP\Command;
+
+class InstallCommand extends \CLIFramework\Command 
+{
+    function brief() { 
+        return 'install flavor to your global flavor path';
+    }
+
+    function execute($args)
+    {
+
+    }
+}
