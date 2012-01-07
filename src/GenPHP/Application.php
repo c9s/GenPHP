75bacf107a12b118491a2dd8f4d6b9d9671aa183
c9s <cornelius.howl@gmail.com>
2012-01-07 11:54:03 +0800

    Add NewCommand.

--- src/GenPHP/Application.php
+++ src/GenPHP/Application.php
@@ -4,6 +4,12 @@ use CLIFramework\Application as CLIApp;
 class Application extends CLIApp
 {
 
+    function init()
+    {
+        $this->registerCommand('new');
+        parent::init();
+    }
+
     function options($opts)
     {
         parent::options( $opts );
---
+++ src/GenPHP/Command/NewCommand.php
@@ -0,0 +1,11 @@
+<?php 
+namespace GenPHP\Command;
+use CLIFramework\Command;
+
+class NewCommand extends Command
+{
+    function execute($args)
+    {
+
+    }
+}
