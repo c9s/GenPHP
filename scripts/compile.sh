ba100afe7c44594d38e21a67763a1e3579be4dfb
c9s <cornelius.howl@gmail.com>
2012-01-07 23:39:36 +0800

    For genphp.phar
    - Add genphp.phar
    - Update compile script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,6 +3,6 @@ onion.phar -d compile \
     --lib src \
     --lib vendor/pear \
     --classloader \
-    --bootstrap scripts/genphp \
+    --bootstrap scripts/genphp.embed \
     --executable \
-    --output genphp
+    --output genphp.phar
