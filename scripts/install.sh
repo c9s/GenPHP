3d6529d96213e06ce609c57eca3eb46cfe5f7553
c9s <cornelius.howl@gmail.com>
2012-01-08 13:00:16 +0800

    Add install.sh script

---
+++ scripts/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir tmp
+cd tmp
+git clone https://c9s@github.com/c9s/GenPHP.git
+cd GenPHP
+./genphp install flavors/flavor
+mkdir ~/bin
+cp -v genphp ~/bin
+if [[ -e ~/.zshrc ]] ; then
+    echo "PATH=\$PATH:~/bin" >> ~/.zshrc
+    source ~/zshrc
+elif [[ -e ~/.bashrc ]] ; then
+    echo "PATH=\$PATH:~/bin" >> ~/.bashrc
+    source ~/.bashrc
+fi
+echo "Done"
