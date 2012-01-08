16f9856284237c3ffdaf17313ce95793f9c96229
c9s <cornelius.howl@gmail.com>
2012-01-08 13:10:54 +0800

    Update install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-mkdir tmp
-cd tmp
-git clone git://github.com/c9s/GenPHP.git
-cd GenPHP
+mkdir -p /var/tmp
+cd /var/tmp
+
+if [[ -e GenPHP ]] ; then
+    cd GenPHP
+    git remote update --prune
+    git pull origin master
+else
+    git clone git://github.com/c9s/GenPHP.git
+    cd GenPHP
+fi
 ./genphp install flavors/flavor
-mkdir ~/bin
+mkdir -p ~/bin
 mkdir -p ~/.genphp/flavors
 cp -v genphp ~/bin
-if [[ -e ~/.zshrc ]] ; then
+if [[ -e ~/.zshrc && -z $(grep GenPHP ~/.zshrc) ]] ; then
+    echo "# GenPHP" >> ~/.zshrc
     echo "export PATH=\$PATH:~/bin" >> ~/.zshrc
-    source ~/.zshrc
 elif [[ -e ~/.bashrc ]] ; then
+    echo "# GenPHP" >> ~/bashrc
     echo "export PATH=\$PATH:~/bin" >> ~/.bashrc
-    source ~/.bashrc
 fi
 echo "Done"
+echo ""
+echo "Please reload your bashrc or zshrc"
+echo ""
+echo "To list genphp flavors:"
+echo "    \$ genphp list "
+echo ""
+echo "To create new flavor:"
+echo "    \$ genphp new flavor YourFlavor"
+echo ""
+echo "Enjoy!"
