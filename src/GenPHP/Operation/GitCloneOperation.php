d1295916655c8f7b862c45aac3b88d4e409aab8e
c9s <cornelius.howl@gmail.com>
2012-01-11 06:54:42 +0800

    logAction

--- src/GenPHP/Operation/GitCloneOperation.php
+++ src/GenPHP/Operation/GitCloneOperation.php
@@ -6,12 +6,13 @@ class GitCloneOperation extends Operation
 {
     function run( $repoUri , $target )
     {
-        $this->logAction('git:clone',$repoUri);
         if( file_exists($target) ) {
+            $this->logAction('git:update',$repoUri);
             Helper::system('git','--git-dir',$target . DIRECTORY_SEPARATOR . '.git','remote','update','--prune');
             Helper::system('git','--git-dir',$target . DIRECTORY_SEPARATOR . '.git','pull');
         }
         else {
+            $this->logAction('git:clone',$repoUri);
             Helper::system('git','clone',$repoUri,$target);
         }
     }
