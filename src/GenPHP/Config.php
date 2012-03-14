8490e12c6cd373ba34e09f2b896b89f59db48ee5
c9s <cornelius.howl@gmail.com>
2012-03-14 20:55:37 +0800

    Implmenet array access interface.

--- src/GenPHP/Config.php
+++ src/GenPHP/Config.php
@@ -1,11 +1,13 @@
 <?php
 namespace GenPHP;
+use ArrayAccess;
 
 if( ! defined('DS') ) {
     define('DS', DIRECTORY_SEPARATOR);
 }
 
 class Config
+    implements ArrayAccess
 {
     public $home;
     public $config;
@@ -36,6 +38,36 @@ class Config
     function _parseConfigFile($configFile)
     {
         return parse_ini_file( $configFile, true );
+    }
+
+
+    
+    public function offsetSet($n,$v)
+    {
+        $this->config[ $n ] = $v;
+    }
+
+    public function offsetExists($n)
+    {
+        return isset($this->config[$n]);
+    }
+
+    public function offsetGet($n)
+    {
+        if( isset($this->config[$n]) )
+            return $this->config[ $n ];
+    }
+
+    public function offsetUnset($n)
+    {
+        unset($this->config[$n]);
+    }
+
+
+    public function __get($n)
+    {
+        if( isset($this->config[$n]) )
+            return $this->config[ $n ];
     }
 
 }
