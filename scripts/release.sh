666de5ba8ceebef5e5175e3b7f3810888f3c1fe1
c9s <cornelius.howl@gmail.com>
2012-05-22 15:44:44 +0800

    Build Release for version  1.1.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 version=$(cat package.ini|grep version|sed -e 's/version =//')
 scripts/compile.sh
-onion.phar -d build
+onion -d build
 git commit -a -m "Build Release for version $version"
