0d47ddcaf1c0e0e2b7c097c812d2fc636a7da583
c9s <cornelius.howl@gmail.com>
2013-03-21 23:59:17 +0800

    update CLIFrameowrk composer.json

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,2 @@
 <?php
-require 'PHPUnit/TestMore.php';
-require 'vendor/pear/Universal/ClassLoader/BasePathClassLoader.php';
-define('ROOT_DIR' , dirname(__DIR__) );
-$loader = new \Universal\ClassLoader\BasePathClassLoader( array( 
-    ROOT_DIR . '/src', 
-    ROOT_DIR . '/vendor/pear'
-));
-$loader->register();
+require 'vendor/autoload.php';
