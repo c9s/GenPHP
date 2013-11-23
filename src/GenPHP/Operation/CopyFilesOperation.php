2f9aacf2f6bf71667a366b7dd2adb65845101135
c9s <cornelius.howl@gmail.com>
2013-11-24 02:04:09 +0800

    implement CopyFilesOperation

--- src/GenPHP/Operation/CopyFilesOperation.php
+++ src/GenPHP/Operation/CopyFilesOperation.php
@@ -5,7 +5,16 @@ use GenPHP\Operation\Helper;
 class CopyFilesOperation extends Operation
 {
 
-
+    public function run($files)
+    {
+        foreach( $files as $file ) {
+            if ( file_exists($file) ) {
+                $this->logger->notice("skip $file",1);
+                continue;
+            }
+            $this->generator->copy( $file , $file);
+        }
+    }
 
 }
 
