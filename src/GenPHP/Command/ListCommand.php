0f15304bcbe41edce8060084f28d06d3df3d1117
c9s <cornelius.howl@gmail.com>
2013-11-24 16:49:13 +0800

    ignore dot files

--- src/GenPHP/Command/ListCommand.php
+++ src/GenPHP/Command/ListCommand.php
@@ -20,13 +20,15 @@ class ListCommand extends \CLIFramework\Command
 
         $paths = Path::get_flavor_paths();
         foreach( $paths as $path ) {
-            if( ! file_exists($path))
+            if ( ! file_exists($path) ) {
                 continue;
+            }
 
-            if( $handle = opendir( $path ) ) {
+            if ( $handle = opendir( $path ) ) {
                 while (false !== ($entry = readdir($handle))) {
-                    if( $entry == '.' || $entry == '..' ) 
+                    if ( $entry[0] == '.' || $entry == '..' )  {
                         continue;
+                    }
                     $logger->info( sprintf("%-10s  %s",$entry, $path), 1 );
                 }
                 closedir($handle);
