f1e898f4f3cb7fda2cca57cc48294f80aea314b5
c9s <cornelius.howl@gmail.com>
2013-11-24 15:32:15 +0800

    require fileutil

--- tests/GenPHP/Flavor/FlavorLoaderTest.php
+++ tests/GenPHP/Flavor/FlavorLoaderTest.php
@@ -2,7 +2,9 @@
 
 class FlavorLoaderTest extends PHPUnit_Framework_TestCase
 {
-    function testLoader()
+    const CHDIR = "tests/root";
+
+    public function testLoader()
     {
         /* use default flavor dirs */
         $loader = new GenPHP\Flavor\FlavorLoader(array( ROOT_DIR . '/tests/flavors' ));
@@ -10,15 +12,55 @@ class FlavorLoaderTest extends PHPUnit_Framework_TestCase
 
         $flavor = $loader->load('license');
         ok( $flavor );
-
-
         return $flavor;
+    }
+
+    public function flavorProvider()
+    {
+        return [
+            ['license', ['MIT'] ]
+        ];
+    }
+
+
+    /**
+     * @dataProvider flavorProvider
+     */
+    public function testFlavors($flavorName, $args)
+    {
+        $loader = new GenPHP\Flavor\FlavorLoader(array( ROOT_DIR . '/tests/flavors', 'flavors' ));
+        ok($loader);
+
+        $flavor = $loader->load($flavorName);
+        ok( $flavor );
+        ok( $flavor->exists() );
+        is( $flavorName, $flavor->getName() );
+
+        $dir = $flavor->getResourceDir();
+        path_ok( $dir , 'Found resource directory' );
+
+        $generator = $flavor->getGenerator();
+        ok( $generator );
+
+        $pwd = getcwd();
+        $this->prepareChdir();
+        chdir(self::CHDIR);
+        $runner = new \GenPHP\GeneratorRunner;
+        $runner->run($generator,$args);
+        chdir($pwd);
+    }
+
+    public function prepareChdir()
+    {
+        if ( ! file_exists(self::CHDIR) ) {
+            mkdir(self::CHDIR,0755,true);
+        }
     }
 
     /**
      * @depends testLoader
      */
-    function testFlavor($flavor) {
+    public function testFlavor($flavor) {
         $generator = $flavor->getGenerator();
         ok( $generator );
         ok( $flavor->exists() );
@@ -30,14 +72,10 @@ class FlavorLoaderTest extends PHPUnit_Framework_TestCase
 
         $pwd = getcwd();
         $root = "tests/root";
-        if( ! file_exists($root) )
+        if ( ! file_exists($root) ) {
             mkdir($root,0755,true);
+        }
         chdir($root);
-
-        $runner = new \GenPHP\GeneratorRunner;
-        $runner->run($generator,array('MIT'));
-        path_ok('LICENSE');
-
         chdir($pwd);
     }
 }
